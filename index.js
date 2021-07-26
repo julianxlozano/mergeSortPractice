fd5f2c6baf423e0a4a0ad0735154b85d6744d001
julianxlozano <julianxlozano@gmail.com>
2021-07-26 18:29:54 -0500

    solved merge sort

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const findMinAndRemoveSorted = (array) =>{
+    return array.shift()
 
+}
+
+const merge = (firstArray,secondArray) =>{
+    let sorted = []
+    while (firstArray.length !=0 && secondArray.length != 0){
+       if (firstArray[0] < secondArray[0]){
+            let currentMin = findMinAndRemoveSorted(firstArray)
+            sorted.push(currentMin)
+       }else{
+            let currentMin = findMinAndRemoveSorted(secondArray)
+            sorted.push(currentMin)
+       }
+    }
+    return sorted.concat(firstArray).concat(secondArray)
+}
+
+const mergeSort = (array) =>{
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    
+    if(array.length < 2){
+      return array
+    } else {
+      return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
